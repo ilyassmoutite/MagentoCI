93cdec02c62608ac63b1c71356956569c608702c
ilyassmoutite <45095556+ilyassmoutite@users.noreply.github.com>
2021-07-01 12:41:33 +0100

    Update 20-m1-composer.sh

--- build/10-pre-process/20-m1-composer.sh
+++ build/10-pre-process/20-m1-composer.sh
@@ -8,8 +8,8 @@ if [ -f "${CHECKOUT_DIR}/composer.json" ]; then
     logvalue "Executing composer install"
     COMPOSER_PATH="$(which composer)"
     (
-          cd ${CHECKOUT_DIR} && $PHP /usr/local/bin/composer install --ignore-platform-reqs --no-interaction --no-plugins --no-dev  --no-progress --no-suggest --prefer-dist --optimize-autoloader &&
-          $PHP $COMPOSER_PATH run-script post-install-cmd -vvv -- --redeploy -n
+        #  cd ${CHECKOUT_DIR} && $PHP /usr/local/bin/composer install --ignore-platform-reqs --no-interaction --no-plugins --no-dev  --no-progress --no-suggest --prefer-dist --optimize-autoloader &&
+        #  $PHP $COMPOSER_PATH run-script post-install-cmd -vvv -- --redeploy -n
        #   ls -alh ${CHECKOUT_DIR}/docs
     )
 fi
